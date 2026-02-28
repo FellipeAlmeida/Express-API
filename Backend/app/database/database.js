06a8d79e4ce577b01b82d12af6d92262056d1f8d
Fellipe Almeida <almeidafellipe237@gmail.com>
2026-02-28 13:20:54 -0300

    fix: database working in production

--- Backend/app/database/database.js
+++ Backend/app/database/database.js
@@ -2,13 +2,25 @@ import pkg from 'pg'
 import { env } from '../config/config.js'
 
 const { Pool } = pkg 
+let config 
 
-const pool = new Pool ({
-    user: env.postgresUser,
-    host: env.postgresHost,
-    database: env.postgresDB,
-    password: env.postgresPassword,
-    port: env.port,
-})
+if (process.env.DATABASE_URL) {
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+            }
+        }
+} else {
+    config = {
+        user: env.postgresUser,
+        host: env.postgresHost,
+        database: env.postgresDB,
+        password: env.postgresPassword,
+        port: env.port,
+    }
+}
+
+let pool = new Pool(config)
 
 export default pool
